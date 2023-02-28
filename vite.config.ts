8139ac87e81a3b6924195759ba4904e0e301b280
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2023-02-28 14:50:42 -0300

    Add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), visualizer({
+    emitFile: true,
+    filename: "stats.html",
+    gzipSize: true,
+  })],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
